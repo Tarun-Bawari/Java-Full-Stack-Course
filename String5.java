06770254442f80f98ed4de66baf19b386956500d
Tarun-Bawari <52121628+Tarun-Bawari@users.noreply.github.com>
2022-08-21 23:21:54 +0530

    Add files via upload

---
+++ String5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class RepeatedCharacter {
+	public String findMaxChar(String str) {
+		char[] array = str.toCharArray();
+        int maxCount = 1;
+        char maxChar = array[0];
+        for(int i = 0, j = 0; i < str.length() - 1; i = j){
+        	int count = 1;
+            while (++j < str.length() && array[i] == array[j]) {
+                count++;
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                maxChar = array[i];
+            }
+        }
+        return (maxChar + " = " + maxCount);
+    }
+}
+
+public class String5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String myStr = sc.nextLine();
+		System.out.println(new RepeatedCharacter().findMaxChar(myStr));
+	}
+}
